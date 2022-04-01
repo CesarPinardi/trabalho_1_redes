bc04bffb9c3466fa273ad212208c5ebb851ae419
César Augusto Pinardi <cesar.pinardi@gmail.com>
2022-04-01 15:13:47 -0300

    end

--- servidor.py
+++ servidor.py
@@ -1,9 +1,9 @@
 from audioop import add
 import json
 import socket
-
 from datetime import datetime
 
+# pegando horario atual para a criacao do server
 
 now = datetime.now()
 
@@ -15,10 +15,6 @@ localPort = 20001
 
 bufferSize = 1024
 
-msgFromServer = "Ola cliente UDP"
-
-bytesToSend = str.encode(msgFromServer)
-
 # criando o socket pra receber
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -27,28 +23,25 @@ UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 UDPServerSocket.bind((localIP, localPort))
 
+# print para reconhecimento que o servidor foi criado
 print("UDP server ouvindo")
 print("Servidor criado as: " + horario_criacao)
 
-json_envio = {}
-
-msg_para_client = 'Mensagem recebida!'
-
-# Listen for incoming datagrams
+# talvez tenha que criar o json aqui fora antes de referenciar la dentro?
 
 while(True):
-    
+    # pegando horario atual para a resposta
     now = datetime.now()
 
     current_time = now.strftime("%H:%M:%S")
 
-
+    # recebendo a resposta do cliente 
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
 
+    # referenciando a resposta (array)
     message = bytesAddressPair[0]
 
     address = bytesAddressPair[1]
-
 
     #transformar message para string
     msg_em_string = message.decode()
@@ -56,20 +49,13 @@ while(True):
     #transformar message para json
     json_client = json.loads(msg_em_string)
 
-    #acessando o json
-
-        # print("\nMensagem do cliente: " + json_client.get("info_to_sent").get("msg"))
-
-        # print("\nAgora: " + json_client.get("info_to_sent").get("timestamp"))
-
-        # print("\nRequest recebido as: " + current_time)
-
-        # print("\nIP cliente: " + json_client.get("info_to_sent").get("ip_origem"))
+    print("Recebido pelo cliente\n")
 
     print((json.dumps(json_client.get("info_to_sent"), indent= len(json_client.get("info_to_sent")))))
 
     print("\n")
 
+    # acessando as keys do json
     clientMsg = json_client.get("info_to_sent").get("msg")
 
     clientIP  = json_client.get("info_to_sent").get("ip_origem")
@@ -78,18 +64,39 @@ while(True):
     
     time_client = json_client.get("info_to_sent").get("timestamp")
 
-    json_envio ['received'] = {'ip_origem': localIP, 
-                                'ip_destino': clientIP, 
-                                'porta_origem': localPort, 
-                                'porta_destino': clientPort, 
-                                'timestamp_msg_original': time_client, 
-                                'timestamp_msg_resposta': current_time,
-                                'mensagem_original': clientMsg,
-                                'mensagem_servidor': msg_para_client}
+    msgfromserver = input("Msg para o cliente: ")
 
+    # ao inves de 2 jsons, um array de jsons com os dois, um com ack e outro com msg desse servidor
 
-    str_encoded = json.dumps(json_envio).encode(encoding = 'UTF-8')
+    json_full = {
+        "received": {
+            'ip_origem': localIP, 
+            'ip_destino': clientIP, 
+            'porta_origem': localPort, 
+            'porta_destino': clientPort, 
+            'timestamp_msg_original': time_client, 
+            'timestamp_msg_resposta': current_time,
+            'ACK': True
+            },
 
+        "send": {
+            'ip_origem': localIP, 
+            'ip_destino': clientIP, 
+            'porta_origem': localPort, 
+            'porta_destino': clientPort, 
+            'timestamp_msg_original': time_client, 
+            'timestamp_msg_resposta': current_time,
+            'msg_client': clientMsg,
+            'msg_server': msgfromserver
+            }
+    }
+    
+    # transformando o json array em bytes
+    str_encoded = json.dumps(json_full).encode(encoding = 'UTF-8')
+    
     # mandando de volta pro client
-
     UDPServerSocket.sendto(str_encoded, address)
+
+
+    
+
